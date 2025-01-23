a06b9a45746c19e1640b70f5ca769c2a067af6ec
LeoG <leogorosito.lg@gmail.com>
2025-01-23 20:12:27 -0300

    nodeJs y live-server

---
+++ bu_code.js
@@ -0,0 +1,137 @@
+/* .button{
+    background-color: #195798;
+    color: white;
+    font-size: 14px;
+    padding: 15px 30px;
+    border: none;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #A8D2FF;
+        color: #195798;
+    }
+} */
+ 
+/* .formulario{
+    margin: 45px 0 60px 0;
+}
+
+.formulario .form-group{
+    padding-bottom: 15px;
+}
+
+.formulario input[type="text"], .formulario textarea{
+    padding: 20px;
+    font-size: 14px;
+    width: 100%;
+    border: 1px #ccc solid;
+    box-sizing:border-box;
+
+    &:focus {
+        background-color: #e5f0fc;
+        outline: none;
+        border: 1px #A8D2FF solid;
+      }
+}
+
+.formulario textarea{
+    height: 200px;
+    resize: none;
+}
+
+#foot{
+    font-size: 14px;
+    color: #aaaaaa;
+    margin-bottom: 55px;
+} */
+
+/* .form-bt{
+    display: flex;
+    width: 100%;
+    justify-content:space-between;
+    align-items: center;
+
+    p{
+        text-align: right;
+        color: #195798;
+    }
+
+    a{
+        color:#195798;
+    }
+} */
+
+/* #main-block{
+    float:left;
+    width: 70%;
+    padding-right: 20px;
+    box-sizing:border-box;
+    margin-bottom: 100px;
+
+}
+
+#sidebar{
+    float:right;
+    width: 30%;
+    padding: 20px;
+    background-color: #646464;
+    color: white;
+    box-sizing:border-box;
+    margin-bottom: 100px;
+} */
+
+
+
+
+
+/* .lista li:first-child{
+    background: red;
+}
+
+.lista li:last-child{
+    background: blue;
+}
+
+.lista li:nth-child(3){
+    background: yellow;
+}
+
+.lista{
+    margin-bottom: 200px;
+} */
+
+/*
+tener en cuenta la seudo clase has
+es una especie de condicional relativo al hijo
+
+.container:has(clase:seudo-clase){
+    acá va una propiedad para el elemento;
+}
+
+Por ejemplo
+(boton-rojo:hover)
+
+podemos modificar otro hijo dentro del padre
+
+.container:has(boton-rojo:hover).hijo{
+    width:200%;
+}
+
+*/
+
+/*
+NESTING
+Podés seleccionar elementos dentro de otro elemento como si fuera JS y aplicar la opci{on de hover dentro de la misma clase
+
+.container{
+    width: 100%;
+
+    .img-radius: 50%;
+
+    &:hover{
+        border-radius: 0;
+    }
+}
+
+
+*/
