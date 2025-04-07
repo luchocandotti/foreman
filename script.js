42a919584f00e2b706884f849333caa9a23a1964
Luciano Candotti <lcandotti@gmail.com>
2025-04-07 18:14:55 -0300

    saqué todo el codigo comentado que ya no servía

--- script.js
+++ script.js
@@ -107,75 +107,6 @@ menumElements.forEach(({ buttonId, sectionId }) => {
  });
 
 
-// const button1 = document.getElementById("about");
-// const about = document.getElementById("seccion-about").getBoundingClientRect();
-
-// button1.addEventListener("click", (event) => {
-//   window.scroll({
-//     top: about.top-100,
-//     behavior: "smooth",
-//   });
-//   console.log(about);
-// });
-
-// const button1m = document.getElementById("about-m");
-
-// button1m.addEventListener("click", (event) => {
-//   window.scroll({
-//     top: about.top-50,
-//     behavior: "smooth",
-//   });
-//   document.getElementById('movil').style.top = "-250px";
-//   document.getElementById('container').style.marginTop = "0";
-//   document.getElementById("m").innerHTML = "<a href='#'>=</a>";
-//   sw = 1;
-// });
-
-// const button2 = document.getElementById("services");
-// const services = document.getElementById("seccion-services").getBoundingClientRect();
-
-// button2.addEventListener("click", (event) => {
-//   window.scroll({
-//     top: services.top-70,
-//     behavior: "smooth",
-//   });
-// });
-
-// const button2m = document.getElementById("services-m");
-
-// button2m.addEventListener("click", (event) => {
-//   window.scroll({
-//     top: services.top-25,
-//     behavior: "smooth",
-//   });
-//   document.getElementById('movil').style.top = "-250px";
-//   document.getElementById('container').style.marginTop = "0";
-//   document.getElementById("m").innerHTML = "<a href='#'>=</a>";
-//   sw = 1;
-// });
-
-// const button3 = document.getElementById("contact");
-// const contact = document.getElementById("seccion-contact").getBoundingClientRect();
-
-// button3.addEventListener("click", (event) => {
-//   window.scroll({
-//     top: contact.top,
-//     behavior: "smooth",
-//   });
-// });
-
-// const button3m = document.getElementById("contact-m");
-
-// button3m.addEventListener("click", (event) => {
-//   window.scroll({
-//     top: services.top+200,
-//     behavior: "smooth",
-//   });
-//   document.getElementById('movil').style.top = "-250px";
-//   document.getElementById('container').style.marginTop = "0";
-//   document.getElementById("m").innerHTML = "<a href='#'>=</a>";
-//   sw = 1;
-// });
 
 //Reubicar el scroll de los servicios
 
@@ -198,54 +129,6 @@ acordionElements.forEach(({ buttonId, sectionId }) => {
     });
   });
 });
-
-
-
-// const button4 = document.getElementById("primero");//
-// const acordion1 = document.getElementById("uno").getBoundingClientRect();
-
-// const button5 = document.getElementById("segundo");
-// const acordion2 = document.getElementById("dos").getBoundingClientRect();
-
-// const button6 = document.getElementById("tercero");
-// const acordion3 = document.getElementById("tres").getBoundingClientRect();
-
-// const button7 = document.getElementById("cuarto");
-// const acordion4 = document.getElementById("cuatro").getBoundingClientRect();
-
-//button4.addEventListener("click", (event) => {
-  //window.scroll({
-    //top: acordion1.top,
-    //behavior: "smooth",
-  //});
-//});
-
-
-
-//button5.addEventListener("click", (event) => {
-//  window.scroll({
-//    top: acordion2.top,
-//    behavior: "smooth",
-//  });
-//});
-
-
-
-// button6.addEventListener("click", (event) => {
-//   window.scroll({
-//     top: acordion3.top,
-//     behavior: "smooth",
-//   });
-// });
-
-
-
-// button7.addEventListener("click", (event) => {
-//   window.scroll({
-//     top: acordion4.top,
-//     behavior: "smooth",
-//   });
-// });
 
 
 
